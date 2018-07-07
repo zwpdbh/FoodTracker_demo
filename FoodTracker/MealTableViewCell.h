610eb5e3222a31a7910b0fa6a9342a2a104cdd71
zwpdbh <hyperion_z@outlook.com>
2018-07-07 15:28:04 +1200

    > Add Images to Your Project

---
+++ FoodTracker/MealTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MealTableViewCell.h
+//  FoodTracker
+//
+//  Created by zwpdbh on 2018/7/7.
+//  Copyright © 2018 Otago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MealTableViewCell : UITableViewCell
+
+@end
---
+++ FoodTracker/MealTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.m
+//  FoodTracker
+//
+//  Created by zwpdbh on 2018/7/7.
+//  Copyright © 2018 Otago. All rights reserved.
+//
+
+#import "MealTableViewCell.h"
+
+@implementation MealTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
